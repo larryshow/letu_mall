c199e8f7a055b5f2a224ceff3ccdc41cfafbef31
oujiangbo <oujiangbo@yw01.com>
2016-08-11 15:31:18 +0800

    解决冲突

--- letu_mall/letu_mall_service_impl/src/main/java/com/letu/mall/App.java
+++ letu_mall/letu_mall_service_impl/src/main/java/com/letu/mall/App.java
@@ -9,7 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("asasdasasasasa");
         System.out.println("changed");
+        System.err.println(",,,,,,,,,,,,,,,,,,,,,,,");
     }
     
 }
